b78d2b75ff0426e4a9bc39ed3aede4f400de23e1
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-09-01 18:46:05 +0300

    Displayed books

--- index.js
+++ index.js
@@ -29,3 +29,37 @@ const getPopular = async () => {
       addPopularLike();
     });
   };
+  const getBooks = async () => {
+    const fetchBooks = await fetch(
+      "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=su6Pt4Vu8bMecCsPP5C5kA09UJyPuwAn"
+    );
+    const fetchedBooks = await fetchBooks.json();
+    console.log(fetchedBooks.results);
+    displayBooks(fetchedBooks.results);
+  };
+  const displayBooks = (Books) => {
+    Books.books.forEach((book) => {
+      if(book.book_image !== null){
+        document.getElementById("top-reads-div").innerHTML += `
+        <div class="each-character-div">
+        <h2 class="byline" >RANK: ${book.rank}</h2>
+        <h2 class="byline">TITLE :${book.title}</h2>
+        <img src=${book.book_image} alt=${book.title}>
+        <p class="description">Author : ${book.author}</p>
+        <p class="description">Description : ${book.description}</p>
+        <div class="like-div">
+        <p class="book-like-count" id=${book.book_uri}>0<p>
+        <i id=${book.book_uri} class="fa fa-thumbs-up book-like-button" aria-hidden="true"></i>
+        </div>
+      </div>
+      
+        
+        
+        `;
+  
+      }
+     
+      addBooksLike();
+      loadBooksLike();
+    });
+  };
